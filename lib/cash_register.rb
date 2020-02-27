7b3d42b9b193562427d6b88f8e4b2727b9225454
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2020-02-27 00:52:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,15 +22,13 @@ def initialize(discount =0)
   end
   
   def apply_discount
-  
-  if discount != 0   
-    
-    self.total = (total((100.0 - discount.to_f)/100)).to_i
-    "After the discouunt has been applied the total is $#{self.total}."
-  else
-    "There is no discount to apply."
+    if discount != 0
+      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+      "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply."
+    end
   end
-end
 
  
 end
